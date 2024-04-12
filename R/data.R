4481c9504b3b3c56cc8ece52e0f8a60d8807d50a
Elinor Sterner <esterner@smith.edu>
2024-04-12 08:53:11 -0400

    Initial commit

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#' @title Metadata for the first 10 xkcd comics
+#'
+#' @format
+#' \describe {
+#' \item{month}{Month of the year the comic was published}
+#' \item{number}{Comic number}
+#' \item{link}{Link to relevant context, source information, or a large version of the comic image. this field is empty for the first 10 comics}
+#' \item{year}{Year the comic was published}
+#' \item{news}{Seemingly random tidbits about the comics}
+#' \item{safe_title}{The title of the comic}
+#'
+#' \item{transcript}{text in the comid}
+#' \item{alt}{Alt text for the image in the web browser}
+#' \item{img}{Alt text for the image in the web browser}
+#' \item{title}{Alt text for the image in the web browser}
+#' \item{day}{day of the month}
+#' }
+#' @source <https://xkcd.com/json.html>
+"xkcd_data"
